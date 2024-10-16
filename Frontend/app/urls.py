ddbd5ab0a1df313228381200068f2189b1744602
bryanthr6 <2988707890101@ingenieria.usac.edu.gt>
2024-10-16 02:14:39 -0600

    Se ageraron diseños de cada opción y se creó un side bar que permite navegar entre las opciones mediante el uso de tailwind css para diseñar esto

--- Frontend/app/urls.py
+++ Frontend/app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='inicio'),
+    path('carga/', views.cargar_archivo, name='carga'),
+    path('estadisticas/', views.ver_grafica, name='estadisticas'),
+    path('pdf/', views.datos_estudiante, name='pdf'),
+    path('procesados/', views.ver_procesados, name='ver_procesados'),  # Nueva URL para ver procesados
 ]
--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -5,3 +5,15 @@ import requests
 
 def index(request):
     return render(request, 'index.html')
+
+def cargar_archivo(request):
+    return render(request, 'cargar.html')
+
+def ver_grafica(request):
+    return render(request, 'grafica.html')
+
+def datos_estudiante(request):
+    return render(request, 'estudiante.html')
+
+def ver_procesados(request):
+    return render(request, 'verprocesados.html')
